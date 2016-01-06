44c138f14f0bf1bdf72dde1cfcf5827c1ece594e
v11 <442629928@qq.com>
2016-01-06 17:26:17 +0800

    11
    11

---
+++ elink-spider/src/main/java/org/elink/spider/hudong/HuDongParser.java
@@ -0,0 +1,9 @@
+package org.elink.spider.hudong;
+
+import java.util.List;
+
+public interface HuDongParser {
+	List<String> getTag();
+	List<String> getSubClass();
+	List<String> getSuperClass();
+}
